dbf04302aa31a1c24ad8830cd6dda0b0b23cec79
mbaumgartenbr <mbaumgartenbr@gmail.com>
2022-05-31 15:54:02 -0300

    Fixes library exports

--- lib/flutter_fancy_tree_view.dart
+++ lib/flutter_fancy_tree_view.dart
@@ -1,5 +1,4 @@
 library flutter_fancy_tree_view;
 
-export 'src/tree_controller.dart';
-export 'src/tree_data_source.dart';
-export 'src/tree_view.dart';
+export 'src/foundation.dart';
+export 'src/widgets.dart';
